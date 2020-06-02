2b780bfb6a60934639b39f7d012e3354335176a7
Kyurenpoto <heal9179@gmail.com>
2020-06-03 02:05:26 +0900

    sentifer_memnager: Add Basic Structure

--- projects/sentifer_memnager/example/main.cpp
+++ projects/sentifer_memnager/example/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "../memnager.h"
+
 int main()
 {
     return 0;
--- projects/sentifer_memnager/memnager.h
+++ projects/sentifer_memnager/memnager.h
@@ -2,7 +2,86 @@
 
 #pragma once
 
+#include <cstddef>
+#include <array>
+#include <variant>
+
 namespace memnager
 {
+    template<size_t power>
+    struct AlignedPtr final
+    {
+        void* ptr = nullptr;
+
+        bool isValid()
+        {
+            return reinterpret_cast<size_t>(ptr) % (1 << power) == 0;
+        }
+    };
+
+    class PoolAllocator
+    {
+        template<size_t BlockSize>
+        struct MemBlock
+        {
+            std::array<std::byte, BlockSize> memory;
+        };
+
+        static constexpr size_t PageSize = 4096;
+        static constexpr size_t CacheLineSize = 64;
+        static constexpr size_t NodeArrayCnt = PageSize / CacheLineSize;
+
+        using Page = MemBlock<PageSize>;
+        using CacheLine = MemBlock<CacheLineSize>;
+        using CacheAlignedPtr = AlignedPtr<CacheLineSize>;
+
+        using Node = std::variant<CacheLine, CacheAlignedPtr>;
+        using NodeArray = std::array<Node, NodeArrayCnt>;
+        using NodeList = std::variant<Page, NodeArray>;
+
+    public:
+        PoolAllocator()
+        {
+            Node* prevNode = nullptr;
+
+            for (size_t i = 0; i < NodeArrayCnt; ++i)
+            {
+                Node& newStack = std::get<NodeArray>(nodes)[i];
+                CacheAlignedPtr& link = std::get<CacheAlignedPtr>(newStack);
+                link.ptr = static_cast<void*>(prevNode);
+                prevNode = static_cast<Node*>(link.ptr);
+            }
+
+            stack = prevNode;
+        }
+
+        CacheAlignedPtr allocChunk()
+        {
+            if (!stack)
+                return CacheAlignedPtr{};
+
+            Node* top = stack;
+            CacheAlignedPtr& link = std::get<CacheAlignedPtr>(*top);
+            stack = static_cast<Node*>(link.ptr);
+
+            CacheAlignedPtr chunk;
+            chunk.ptr = static_cast<void*>(top);
+
+            return chunk;
+        }
+
+        void freeChunk(CacheAlignedPtr ptr)
+        {
+            Node* top = static_cast<Node*>(ptr.ptr);
+            CacheAlignedPtr& link = std::get<CacheAlignedPtr>(*top);
+            link.ptr = static_cast<void*>(stack);
+            stack = top;
+        }
+
+    private:
+        Node* stack;
+        NodeList nodes;
+    };
+
     void init(size_t nArena = 4);
 }
