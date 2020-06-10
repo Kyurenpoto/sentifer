6012f1a9401f0d8db6defe05a8272f23f8ec0ea0
Kyurenpoto <heal9179@gmail.com>
2020-06-11 03:43:47 +0900

    Reconstruct project structure: sentifer

--- projects/sentifer_mtbase/include/sentifer_mtbase/mtbase.hpp
+++ projects/sentifer_mtbase/include/sentifer_mtbase/mtbase.hpp
@@ -1,1 +1,59 @@
 #pragma once
+
+#include <memory_resource>
+
+#include "mimalloc.h"
+
+namespace mtbase
+{
+    inline namespace memory_managers
+    {
+        struct mi_memory_resource :
+            public std::pmr::memory_resource
+        {
+            mi_memory_resource() :
+                heap{ mi_heap_new() }
+            {}
+
+            virtual ~mi_memory_resource()
+            {
+                mi_heap_destroy(heap);
+            }
+
+        private:
+            virtual void* do_allocate(std::size_t bytes, std::size_t alignment) override
+            {
+                return mi_heap_zalloc_aligned(heap, bytes, alignment);
+            }
+
+            virtual void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override
+            {
+                return mi_free(p);
+            }
+
+            virtual bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override
+            {
+                return this == &other;
+            }
+
+            mi_heap_t* heap;
+        };
+
+        struct thread_local_heap :
+            public mi_memory_resource
+        {
+
+        };
+
+        struct object_heap :
+            public mi_memory_resource
+        {
+
+        };
+
+        struct manage_heap
+        {
+
+        };
+    }
+}
