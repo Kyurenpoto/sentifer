e043823609e0f09ba56e203b138395cff69f92a3
Kyurenpoto <heal9179@gmail.com>
2020-06-11 13:02:15 +0900

    sentifer_mtbase: Implemented clocks

--- projects/sentifer_mtbase/include/sentifer_mtbase/mtbase.hpp
+++ projects/sentifer_mtbase/include/sentifer_mtbase/mtbase.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory_resource>
+#include <chrono>
 
 #include "mimalloc.h"
 
@@ -55,5 +56,54 @@ namespace mtbase
         {
 
         };
+    }
+
+    inline namespace clocks
+    {
+        template<class Clock>
+        struct clock_base
+        {
+            clock_base() noexcept
+            {
+                startSinceEpoch = baseSinceEpoch = Clock::now().time_since_epoch();
+            }
+
+            virtual ~clock_base() noexcept
+            {}
+
+            std::chrono::nanoseconds nowFromBase() noexcept
+            {
+                return now() - baseSinceEpoch;
+            }
+
+            void setBase(std::chrono::nanoseconds base) noexcept
+            {
+                baseSinceEpoch = base;
+            }
+
+            void resetBase() noexcept
+            {
+                baseSinceEpoch = startSinceEpoch;
+            }
+
+        private:
+            std::chrono::nanoseconds now() noexcept
+            {
+                return Clock::now().time_since_epoch();
+            }
+
+        private:
+            std::chrono::nanoseconds startSinceEpoch;
+            std::chrono::nanoseconds baseSinceEpoch;
+        };
+
+        inline namespace instanced
+        {
+            using thread_local_clock = clock_base<std::chrono::steady_clock>;
+            using global_clock = clock_base<std::chrono::system_clock>;
+
+            static thread_local thread_local_clock LocalClock;
+            static global_clock GlobalClock;
+        }
     }
 }
