c4711975cf3c6b0a8c80269642c3b48539c57fbc
Kyurenpoto <heal9179@gmail.com>
2020-06-26 11:34:44 +0900

    sentifer_mtbase: Changed default test main

--- projects/sentifer_mtbase/tests/main.cpp
+++ projects/sentifer_mtbase/tests/main.cpp
@@ -6,25 +6,21 @@
 int main(int argc, char** argv) {
     doctest::Context context;
 
-    // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
-
-    // defaults
-    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
-    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
-    context.setOption("order-by", "name");            // sort the test cases by their name
+    context.setOption("abort-after", 5);
+    context.setOption("order-by", "file");
 
     context.applyCommandLine(argc, argv);
 
-    // overrides
-    context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
+    context.setOption("no-breaks", true);
 
-    int res = context.run(); // run
+    int res = context.run();
 
-    if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-        return res;          // propagate the result of the tests
+    if (context.shouldExit())
+        return res;
 
-    int client_stuff_return_code = 0;
-    // your program - if the testing framework is integrated in your production code
+    context.clearFilters();
 
-    return res + client_stuff_return_code; // the result from doctest is propagated here as well
+    int client_stuff_return_code = EXIT_SUCCESS;
+
+    return res + client_stuff_return_code;
 }
