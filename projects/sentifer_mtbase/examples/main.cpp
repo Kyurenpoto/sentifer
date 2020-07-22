e1f848c6dbdb96d5c1a126b108a25bf96ce3cc6f
Kyurenpoto <heal9179@gmail.com>
2020-07-23 01:59:01 +0900

    sentifer_mtbase: Added basic benchmark

--- projects/sentifer_mtbase/examples/main.cpp
+++ projects/sentifer_mtbase/examples/main.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+
 #include <fmt/core.h>
 
 #include "sentifer_mtbase/mtbase.h"
@@ -5,8 +7,9 @@
 void warmup()
 {
     int x = 0x12345678;
-    for (int i = 0; i < 1'000'000'000; ++i)
-        x ^= (x * x);
+    for (int j = 0; j < 10; ++j)
+        for (int i = 0; i < 1'000'000'000; ++i)
+            x ^= (x * x);
 }
 
 int main()
@@ -14,6 +17,20 @@ int main()
     fmt::print("Warm up process...\n");
     warmup();
     fmt::print("Warm up complete\n");
+
+    mtbase::task_t task[1'000'000];
+    mtbase::task_wait_free_deque<1'000'000> deq{ std::pmr::get_default_resource() };
+
+    fmt::print("1 thread only push_back task...\n");
+
+    std::chrono::nanoseconds begin = std::chrono::steady_clock::now().time_since_epoch();
+
+    for (int i = 0; i < 1'000'000; ++i)
+        deq.push_back(&task[i]);
+
+    std::chrono::nanoseconds end = std::chrono::steady_clock::now().time_since_epoch();
+
+    fmt::print("Complete: {}ms\n", std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count());
 
     return 0;
 }
