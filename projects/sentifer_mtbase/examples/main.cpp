806ff9e03e72a0312ea05b7ee333c1370c5ec7a1
Kyurenpoto <heal9179@gmail.com>
2020-07-22 17:59:42 +0900

    sentifer: Added submodule 'fmt'

--- projects/sentifer_mtbase/examples/main.cpp
+++ projects/sentifer_mtbase/examples/main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
+#include <fmt/core.h>
 
 #include "sentifer_mtbase/mtbase.h"
 
+void warmup()
+{
+    int x = 0x12345678;
+    for (int i = 0; i < 1'000'000'000; ++i)
+        x ^= (x * x);
+}
+
 int main()
 {
+    fmt::print("Warm up process...\n");
+    warmup();
+    fmt::print("Warm up complete\n");
+
     return 0;
 }
