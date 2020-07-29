81b02c5ef358c9b9122da80c8b8abb44e164ea78
Kyurenpoto <heal9179@gmail.com>
2020-07-29 19:19:32 +0900

    sentifer_mtbase: Separated warm up from benchmark

--- projects/sentifer_mtbase/examples/main.cpp
+++ projects/sentifer_mtbase/examples/main.cpp
@@ -6,11 +6,12 @@
 
 #include "sentifer_mtbase/mtbase.h"
 
-constexpr size_t sz = 10'000'000;
+constexpr size_t SZ = 10'000'000;
+constexpr size_t SZ_WARM_UP = 10'000;
 
-static mtbase::task_t task[sz];
+static mtbase::task_t task[SZ];
 static std::pmr::synchronized_pool_resource res;
-static mtbase::task_wait_free_deque<sz> deq{ &res };
+static mtbase::task_wait_free_deque<SZ> deq{ &res };
 
 void only_push_front(int threadId, int idxBegin, int idxEnd)
 {
@@ -74,7 +75,7 @@ void only_pop_back(int threadId, int idxBegin, int idxEnd)
 
 void test_thread_n(int n, void(*f)(int, int, int))
 {
-    int m = sz / n;
+    int m = SZ / n;
     std::vector<std::thread> t;
     t.reserve(n);
 
@@ -90,7 +91,7 @@ void test_thread_n(int n, void(*f)(int, int, int))
 
 void fullDeq()
 {
-    for (int i = 0; i < sz; ++i)
+    for (int i = 0; i < SZ; ++i)
     {
         bool result = deq.push_front(&task[i]);
     }
@@ -104,6 +105,30 @@ void emptyDeq()
 void identityDeq()
 {
 
+}
+
+void warmup()
+{
+    fmt::print("warm up begin\n");
+
+    {
+        std::thread t{ []() { only_push_back(0, 0, SZ_WARM_UP); } };
+        t.join();
+    }
+    {
+        std::thread t{ []() { only_pop_back(0, 0, SZ_WARM_UP); } };
+        t.join();
+    }
+    {
+        std::thread t{ []() { only_push_front(0, 0, SZ_WARM_UP); } };
+        t.join();
+    }
+    {
+        std::thread t{ []() { only_pop_front(0, 0, SZ_WARM_UP); } };
+        t.join();
+    }
+
+    fmt::print("warm up end\n");
 }
 
 void test_threads_to_n(int n, void(*f)(int, int, int), void (*post)())
@@ -126,10 +151,7 @@ void test_threads_to_n(int n, void(*f)(int, int, int), void (*post)())
 
 int main()
 {
-    fmt::print("warm up begin\n");
-    test_thread_n(1, only_push_back);
-    emptyDeq();
-    fmt::print("warm up end\n");
+    warmup();
 
     fmt::print("only_push_front begin\n");
     test_threads_to_n(4, only_push_front, emptyDeq);
